9195fa1cf44ab06619967ab6c24cd38a594956d2
Robarto <robartpetrov@gmail.com>
2022-11-01 17:28:30 +0200

    Added Base Apparel

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector("button");
+const errorMsg = document.querySelector("#error-message");
+const errorImg = document.querySelector("#error");
+const input = document.querySelector("#email");
+
+button.addEventListener("click", handleClick);
+
+let validRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+function handleClick() {
+  if (input.value.match(validRegex)) {
+    input.value = "";
+    button.classList.remove("pressed");
+    errorMsg.classList.remove("show");
+    errorImg.classList.remove("show");
+    input.classList.remove("show-error");
+  } else {
+    button.classList.add("pressed");
+    errorMsg.classList.add("show");
+    errorImg.classList.add("show");
+    input.classList.add("show-error");
+  }
+}
